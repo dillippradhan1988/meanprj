cad7baaf60af0da715ebb2564f302bde5ec8ecdd
dillippradhan1988 <dillippradhan1988@gmail.com>
2016-06-22 07:08:20 +0530

    lcl

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ var app                         =   express(db);
 var passport 					= 	passport();
 
 //app.set('port', process.env.PORT || 3000);
-app.listen(process.env.PORT);
+//app.listen(process.env.PORT);
+
+app.listen(3000,'127.0.0.1');
 
 module.exports                  =   app;
 
-console.log('Server running at http://localhost:'+process.env.PORT);
+console.log('Server running at http://localhost:3000');
+//console.log('Server running at http://localhost:'+process.env.PORT);
